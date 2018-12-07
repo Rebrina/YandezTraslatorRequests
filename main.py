8b1212e8c4f6fd7f4ccb481032d18261a20d82ea
Rebrina <feurioster@gmail.com>
2018-12-08 00:01:50 +0300

    1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def translate_it(text_for_translate, file_translated, lang, lang_to_translate):
     """
     YANDEX translation plugin
@@ -18,37 +19,37 @@ def translate_it(text_for_translate, file_translated, lang, lang_to_translate):
     key = 'trnsl.1.1.20181206T210101Z.0c17b40d4f148017.89e0ac198607ce8699dad017ab58961bd2e4fe3f'
 
     lang = lang + '-' + lang_to_translate
-    
+
     params = {
         'key': key,
         'lang': lang,
         'text': text_for_translate
     }
-    #response = requests.get(url, params=params).json()
+    # response = requests.get(url, params=params).json()
     response = requests.post(url, params=params, timeout=30).json()
     a = response.get('text', [])
-    with open(file_translated) as f:
+    with open(file_translated, 'w') as f:
         f.write(a[0])
     print(a)
-    #return ' '.join(response.get('text', []))
+    # return ' '.join(response.get('text', []))
 
-#взять файл из текущей директории (путь к файлам) и передать в функцию
+
+# взять файл из текущей директории (путь к файлам) и передать в функцию
 
 if __name__ == "__main__":
-    file_name = input('Введите путь к файлу текста')
-    file_translated = input('Введите путь для сохранения файла перевода')
-    lang = input('Введите язык, с которого нужно перевести (например, de,es,fr')
-    lang_to_translate =input('Введите язык, на который нужно перевести')
+    file_name = input('Введите путь к файлу текста ')
+    file_translated = input('Введите название файлаЮ куда будет записан перевод')
+    lang = input('Введите язык, с которого нужно перевести (например, de,es,fr ')
+    lang_to_translate = input('Введите язык, на который нужно перевести (например, ru) ')
 
     text_for_translate = ''
 
-    with open(file_name, encoding="utf-8") as f: 
+    with open(file_name, encoding="utf-8") as f:
         for line in f:
             if line > '':
                 text_for_translate = text_for_translate + line.strip() + ''
 
-    a = translate_it(text_for_translate, file_translated, lang, lang_to_translate)
-   # print(a)
+    translate_it(text_for_translate, file_translated, lang, lang_to_translate)
 
 
 
